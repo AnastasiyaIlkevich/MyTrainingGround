edd847777437a10a485ef7e1b7f9fc2dca9e6272
AnastasiyaIlkevich <sirin199@yandex.ru>
2022-07-20 13:49:30 +0300

    OvO HW OOP new Circle

---
+++ src/OvO/ClassOOP/HW/ClassCircle/Circle.java
@@ -0,0 +1,32 @@
+package OvO.ClassOOP.HW.ClassCircle;
+
+public class Circle {
+
+    static double Pi = Math.PI;
+    private double radius;
+    private double space;
+    private double circleLength;
+
+    public Circle(double radius) {
+        this.radius = radius;
+        this.space = spaceCalculation(radius);
+        this.circleLength = circleLengthCalculation(radius);
+    }
+
+    private double circleLengthCalculation(double radius) {
+        return  2 * Pi * radius;
+    }
+
+    private double spaceCalculation(double radius) {
+        return Pi * Math.pow(radius, 2);
+    }
+
+    @Override
+    public String toString() {
+        return "Circle{" +
+                "radius=" + radius +
+                ", space=" + space +
+                ", circleLength=" + circleLength +
+                '}';
+    }
+}
---
+++ src/OvO/ClassOOP/HW/ClassCircle/Runner.java
@@ -0,0 +1,14 @@
+package OvO.ClassOOP.HW.ClassCircle;
+
+import java.util.Scanner;
+
+public class Runner {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the radius of the circle -> ");
+        Circle circle = new Circle(scanner.nextDouble());
+        System.out.println(circle);
+    }
+}
