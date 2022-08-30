1a4497d61196db9ca5c40d3c34dfd911280adec8
AnastasiyaIlkevich <sirin199@yandex.ru>
2022-08-30 11:00:01 +0300

    Thread (run)

---
+++ src/OvO/Thread/MetodRun/MyThread.java
@@ -0,0 +1,23 @@
+package OvO.Thread.MetodRun;
+
+import java.util.Date;
+
+public class MyThread extends Thread{
+
+    public MyThread(String name) {
+        super(name);
+    }
+
+    @Override
+    public void run(){
+        System.out.printf("%s started... \n", Thread.currentThread().getName());
+        System.out.println(new Date());
+        try{
+            Thread.sleep(500);
+        } catch (InterruptedException exception){
+            System.err.println("Thread has been interrupted");
+        }
+        System.out.printf("%s fiished... \n", Thread.currentThread().getName());
+    }
+
+}
---
+++ src/OvO/Thread/MetodRun/ThreadEx.java
@@ -0,0 +1,26 @@
+package OvO.Thread.MetodRun;
+
+public class ThreadEx {
+    public static void main(String[] args) {
+        Thread thread = Thread.currentThread();
+        System.out.println(thread + " -> thread");
+        System.out.println(thread.getName() + " -> thread.getName()");
+        System.out.println(thread.getPriority() + " -> thread.getPriority()");
+        System.out.println(thread.getThreadGroup().getName() + "-> thread.getThreadGroup().getName()");
+
+        System.out.println("_______________________________________________");
+        System.out.println("Main thread started...");
+        new MyThread("My Thread").start();
+
+        for (int i = 1; i < 10; i++) {
+            MyThread myThread = new MyThread("My Thread" + i);
+            myThread.start();
+            try {
+                myThread.join();
+            } catch (InterruptedException e) {
+                System.err.printf("%s has been interrupted", myThread.getName());
+            }
+        }
+        System.out.println("Main thread finished...");
+    }
+}
